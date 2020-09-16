0e44d5f7e8b738fd63e084a92106325095f1aa3b
Vikas4853 <71367368+Vikas4853@users.noreply.github.com>
2020-09-16 16:04:27 +0530

    Update FristGit.java

--- GITHUB_PROJECT/src/com/cjc/FristGit.java
+++ GITHUB_PROJECT/src/com/cjc/FristGit.java
@@ -7,6 +7,10 @@ public class FristGit {
 		System.out.println("A method");
 	}
 
+	public void B(){
+	System.out.println("B method created in git and pull ");	
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Run git project");
